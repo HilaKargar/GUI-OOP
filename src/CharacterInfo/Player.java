50b8b4b49c72758452b96bdaf3540400d48e7ed2
hila <hilakargar@gmail.com>
2024-04-04 22:21:32 +0200

    Enemy class and CombatInteface added.

--- src/CharacterInfo/Player.java
+++ src/CharacterInfo/Player.java
@@ -1,4 +1,12 @@
 package CharacterInfo;
 
-public class Player {
+import GUI.TextAdventure;
+
+public class Player extends Characters implements CombatInterface{
+    public Player(String name, int health, int damage, TextAdventure game, int level){
+        super(name,health,damage,game,1);
+        this.xp = 0;
+        this.maxXp = 50;
+        this.inventory = new Inventory<Item<?>>(50,game);
+    }
 }
