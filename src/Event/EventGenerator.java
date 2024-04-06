a0ef17db41d65b9cb8f575976842782ee39bed8f
hila <hilakargar@gmail.com>
2024-04-06 23:33:58 +0200

    EventGenerator class added.

---
+++ src/Event/EventGenerator.java
@@ -0,0 +1,27 @@
+package Event;
+
+import GUI.TextAdventure;
+
+import java.util.Random;
+
+public class EventGenerator {
+    private static String[] eventPool = {"enemy","dungeon","treasure","nothing"};
+    public static String trigger(TextAdventure game){
+        Random r = new Random();
+        int rNumber = r.nextInt(eventPool.length);
+        String randomEvent = eventPool[rNumber];
+        switch(randomEvent){
+            case "enemy":
+                Fight.trigger(game);
+                return "Would you like to fight?";
+            case "dungeon":
+                Dungeon.trigger(game);
+                return "you found a dungeon";
+            case "treasure":
+                game.setEvent(Treasure.trigger(game));
+                return "Would you like to open it?";
+            default:
+                return "you didnt find anything";
+        }
+    }
+}
