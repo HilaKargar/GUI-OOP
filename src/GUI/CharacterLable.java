4fb982315c7db21ce60abcb741ec534fb4be14f2
hila <hilakargar@gmail.com>
2024-05-28 16:05:16 +0200

    CharacterLable class added. InventoryBox class added. LevelUp class added

---
+++ src/GUI/CharacterLable.java
@@ -0,0 +1,52 @@
+package GUI;
+
+
+import CharacterInfo.*;
+
+import javax.swing.*;
+
+public class CharacterLable extends JLabel {
+    private Player player;
+    private Enemy enemy;  // Add an Enemy reference
+
+    public CharacterLabel(Player player,Enemy enemy) {
+        this.player = player;
+        if(enemy!=null){
+            this.enemy = enemy;
+        }
+        updateCharacterInfo();
+    }
+
+    public void setPlayer(Player player) {
+        this.player = player;
+        updateCharacterInfo();
+    }
+
+    public void setEnemy(Enemy enemy) {  // New method to set enemy
+        this.enemy = enemy;
+        updateCharacterInfo();
+    }
+
+    public void updateCharacterInfo() {
+        StringBuilder info = new StringBuilder();
+        if (player != null) {
+            info.append(String.format("%s | Level: %d | Damage: %d | Health: %d / %d | XP: %d / %d" ,
+                    player.getName(),
+                    player.getLevel(),
+                    player.getAttackDamage(),
+                    player.getCurHealth(),
+                    player.getMaxHealth(),
+                    player.getXp(),
+                    player.getMaxXp()));
+        }
+        if (enemy != null) {  // If there is an enemy, append its information
+            info.append(String.format("     Enemy: %s | Level: %d | Damage: %d | Health: %d / %d",
+                    enemy.getName(),
+                    enemy.getLevel(),
+                    enemy.getAttackDamage(),
+                    enemy.getCurHealth(),
+                    enemy.getMaxHealth()));
+        }
+        setText(info.toString());  // Update the text of the label
+    }
+}
